1d86873670d3275a230b46b7752c3742eaa23638
admin <admin@DESKTOP-1AIRQ11>
2019-06-20 11:12:17 +0900

    123

--- KangSpringMonoProject/src/main/java/com/kang/controller/RestAjaxController.java
+++ KangSpringMonoProject/src/main/java/com/kang/controller/RestAjaxController.java
@@ -44,7 +44,8 @@ public class RestAjaxController {
 			String saveFileName = null;
 			try {
 				saveFileName = uploadFile(file.getOriginalFilename(), file.getBytes());
-				
+				System.out.println("12312312312");
+				int a = 123;
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
